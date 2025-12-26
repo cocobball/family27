f1dfce2e8d0cfdf42d004be249de11f42a6f3286
cocobball <corymasri@gmail.com>
2025-12-26 03:36:07 +0000

    Add server/app.js and bootstrap storage init in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import storage from "./storage/index.js";
+
 await storage.initStorage();
 
 console.log("DATA_DIR=", storage.getDataDir());
 console.log("UPLOAD_DIR=", storage.getUploadDir());
+
+await import("./app.js");
